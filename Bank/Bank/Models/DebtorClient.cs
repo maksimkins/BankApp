5deb15091f48c11af2285b42b000e614449b19c7
maksimkins <maks.aleshkov.04@gmail.com>
2023-10-04 01:12:34 +0400

    Add Models

---
+++ Bank/Bank/Models/DebtorClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Models;
+
+public class DebtorClient : LoanClient
+{
+    public decimal DebtToReturn { get; set; } = 0;
+
+    public DebtorClient()
+    {
+        this.DebtToReturn += (base.Loan - base.PaidPartOfLoan);
+        this.DebtToReturn += (base.Loan - base.PaidPartOfLoan) * base.Perecents / 100;
+        this.DebtToReturn += this.DebtToReturn * 5;
+    }
+}
---
+++ Bank/Bank/Models/LoanClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Models
+{
+    public class LoanClient: Client // delete from table if Loan == PaidPartOfLoan
+    {
+        public decimal Loan { get; set; }
+        public decimal PaidPartOfLoan { get; set; }
+        public int Month { get; set; }
+        public int Perecents { get; set; } // 20 15 12 %
+        public DateTime Deadline { get; set; } // +6 +12 +24 month
+        public DateTime PeriodOfPayment { get; set; } // 1 month
+    }
+}
